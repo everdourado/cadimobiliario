d2a922214750fce0a3700d13f0c436ea5bac705f
EverDourado <evertondmcf@hotmail.com>
2023-11-25 16:23:11 -0300

    feat: conexão com banco de dados MongoDB Atlas

---
+++ src/database/db.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose')
+
+const connectDatabase = () => {
+    console.log("Conectando com banco de dados")
+
+    mongoose.connect("mongodb+srv://cadimobiII:1eeEX456785@cluster0.i33i3hs.mongodb.net/?retryWrites=true&w=majority")
+    .then(() => console.log("MongoDB Atlas Conectado"))
+    .catch((error) => console.log(error))
+}
+
+module.exports = connectDatabase
