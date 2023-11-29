2a4cbba80644b0035e28a40663ea4ba0547aafdf
EverDourado <evertondmcf@hotmail.com>
2023-11-28 23:42:37 -0300

    try-catch: middlewares validId e validUser

--- src/middlewares/global.middlewares.js
+++ src/middlewares/global.middlewares.js
@@ -5,29 +5,37 @@ const userService = require("../services/user.service");
 //funções de interceptação, vai interceptar a chamada entre a rota e a função de callback
 
 const validId = (req, res, next) => {
-    const id = req.params.id
-    
-    if(!mongoose.Types.ObjectId.isValid(id)){
-        return res.status(400).send({ message: "Id inválido" })
-    }
+    try {
+        const id = req.params.id
 
-    next()
+        if (!mongoose.Types.ObjectId.isValid(id)) {
+            return res.status(400).send({ message: "Id inválido" })
+        }
+
+        next()
+    } catch (err) {
+        res.status(500).send({ message: err.message })
+    }
 };
 
 const validUser = async (req, res, next) => {
-    const id = req.params.id;
+    try {
+        const id = req.params.id;
 
-    const user = await userService.findByIdService(id);
+        const user = await userService.findByIdService(id);
 
-    if (!user) {
-        return res.status(400).send({ message: "Usuário não encontrado "})
+        if (!user) {
+            return res.status(400).send({ message: "Usuário não encontrado " })
+        }
+
+        //requisição a ser enviada para próxima função vai conter id e user
+        req.id = id;
+        req.user = user;
+
+        next();
+    } catch (err) {
+        res.status(500).send({ message: err.message })
     }
-
-    //requisição a ser enviada para próxima função vai conter id e user
-    req.id = id;
-    req.user = user;
-
-    next()
 };
 
 module.exports = { validId, validUser }
