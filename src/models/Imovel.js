d829a2efe8967ea0ba63d884c8715ca010a51d16
EverDourado <evertondmcf@hotmail.com>
2023-12-02 00:22:37 -0300

    Create Imovel Schema

---
+++ src/models/Imovel.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const ImovelSchema = new mongoose.Schema({
+    cidade: {type: String, required: true},
+    bairro: {type: String, required: true},
+    rua: {type: String, required: true},
+    numero: {type: Number, required: true},
+
+    tipoDeImovel: {type: String, required: true},
+    tipoDeNegocio: {type: String, required: true},
+    atualDisponibilidade: {type: String, required: true},
+    telefone: {type: Number, required: true},
+    dataAnuncio: {type: Date, default: Date.now()},
+    user: {type: mongoose.Schema.Types.ObjectId, ref: "User", require: true}
+});
+
+const Imovel = mongoose.model("Imovel", NewsSchema)
+
+export default Imovel
