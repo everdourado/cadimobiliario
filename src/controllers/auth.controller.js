85eb3f3f4782e947cb054e573458321994d52119
EverDourado <evertondmcf@hotmail.com>
2023-12-01 21:08:47 -0300

    Login - validação email e password

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,5 +1,25 @@
+import bcrypt from 'bcryptjs';
+import { loginService } from '../services/auth.service.js';
+
 const login = async (req, res) => {
-    res.send("Login ok");
+    const  { email, password } = req.body;
+
+    try {
+        const user = await loginService(email);
+        
+        const passwordIsValid = bcrypt.compareSync(password, user.password)
+        
+        console.log(passwordIsValid)
+
+        res.send(user);
+    } catch (err) {
+        res.status(500).send(err.message)
+    }
+    
+
+   
+    
+    
 }
 
 export { login }
