9f4d8d061563f569e71811d1f30ab7b7f8b1d5d9
EverDourado <evertondmcf@hotmail.com>
2023-12-02 21:40:21 -0300

    Url Paginação

--- src/controllers/imovel.controller.js
+++ src/controllers/imovel.controller.js
@@ -61,15 +61,42 @@ const findAll = async (req, res) => {
 
     const imovel = await findAllService(offset, limit)
     const total = await countImovel();
-    console.log(total)
+    const currentUrl = req.baseUrl;
 
     const next = offset + limit
-    const nextUrl = next < total
+    const nextUrl =
+    next < total ? `${currentUrl}?limit=${limit}&offset=${next}` : null;
+
+    const previous = offset - limit < 0 ? null : offset - limit;
+    const previousUrl = previous != null ? `${currentUrl}?limit=${limit}&offset=${previous}` : null;
 
     if (imovel.length === 0) {
         return res.status(400).send({ message: "Não existe imóveis registrados" })
     }
-    res.send(imovel)
+    //ENVIAR PARA O CLIENTE PARA MANIPULAÇÃO NO FRONTEND
+    res.send({
+        nextUrl,
+        previousUrl,
+        limit,
+        offset,
+        total,
+
+        results: imovel.map(item => ({
+            id: item._id,
+            cidade: item.cidade,
+            bairro: item.bairro,
+            rua: item.rua,
+            numero: item.numero,
+            tipoDeImovel: item.tipoDeImovel,
+            tipoDeNegocio: item.tipoDeNegocio,
+            atualDisponibilidade: item.atualDisponibilidade,
+            telefoneContato: item.telefoneContato,
+            name: item.user.name,
+            username: item.user.username,
+            userAvatar: item.user.avatar
+
+        }))
+    })
 }
 
 export { create, findAll };
